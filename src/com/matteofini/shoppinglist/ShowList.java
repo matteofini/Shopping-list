d1210006b6a57415f127ea210ab16959fa30b55b
matteofini <mf.calimero@gmail.com>
2011-11-07 10:37:09 +0100

    modified gpl3 header

--- src/com/matteofini/shoppinglist/ShowList.java
+++ src/com/matteofini/shoppinglist/ShowList.java
@@ -4,7 +4,7 @@
  *  Year: 2011
  *  
  *	This file is part of "Shopping List".
- *	"Liturgia delle Ore" is free software: you can redistribute it and/or modify
+ *	"Shopping List" is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
